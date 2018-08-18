c752829a0b18504777e81dfe5e704a9ea6367adb
duanyong <duanyong@kuaishou.com>
2018-08-18 16:25:58 +0800

    step2

--- src/main/java/com/wdm/baby/Book.java
+++ src/main/java/com/wdm/baby/Book.java
@@ -16,6 +16,14 @@ public class Book {
         this.name = name;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return "Book{" +
